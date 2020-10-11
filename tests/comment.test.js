108b09b4cd5de745dd62bfd3ce5980d7f9bbd618
Kluddizz <flhansen.93@gmail.com>
2020-10-11 23:22:45 +0200

    Add test for delete a comment.

--- tests/comment.test.js
+++ tests/comment.test.js
@@ -1,7 +1,36 @@
 const db = require("../db");
 const fetch = require("node-fetch");
 const { post, comment } = require("./helpers/dummies");
+const {
+  insertPost,
+  insertComment,
+  deletePost,
+  deleteComment,
+} = require("./helpers/functions");
 
 describe("Comments", () => {
-  test("Delete a comment", async () => {});
+  let postId = null;
+
+  beforeAll(async () => {
+    postId = await insertPost(post);
+  });
+
+  afterAll(async () => {
+    await deletePost(post);
+    postId = null;
+  });
+
+  test("Delete a comment", async () => {
+    expect.assertions(1);
+    const commentId = await insertComment(comment, postId);
+
+    const request = await fetch(`http://localhost:5002/comment/${commentId}`, {
+      method: "DELETE",
+    });
+
+    const response = await request.json();
+    await deleteComment(commentId);
+
+    expect(response.status).toBe(200);
+  });
 });
